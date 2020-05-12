141f468e55eee74ffccd8ad62294a834ba2085a9
Insidium <ryanchallen@outlook.com>
2020-05-12 12:20:06 +1000

    Added new scroll animations to transition between slides, corrected issues with slide timing and img stuttering, made covering div a bit higher to help animation, allowed links to be clickable even on scroll

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   //initialise Controller
@@ -9,7 +10,7 @@ function animateSlides() {
   //select slide nav elements
   const nav = document.querySelectorAll(".nav-header");
   //loop over each slide
-  sliders.forEach((slide) => {
+  sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -17,7 +18,8 @@ function animateSlides() {
     const slideTl = gsap.timeline({
       defaults: { duration: 1, ease: "power2.InOut" },
     });
-    //addimng GSAP animations
+
+    //adding GSAP animations
     //reveals img horiz from 0 to full width
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     //presents img as 2x scale then scales to 1x at the same time as above reveal
@@ -26,6 +28,54 @@ function animateSlides() {
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
     //reveals nav vertic from -100 to normal position at 0.5 duration after above
     slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+
+    //create scroll scene
+    slideScene = new ScrollMagic.Scene({
+      //trigger slide element
+      triggerElement: slide,
+      //trigger position at quarter of slide
+      triggerHook: 0.25,
+      //after animation run, don't run again on reverse scroll
+      reverse: false,
+    })
+      //set to run on each slide
+      .setTween(slideTl)
+      //adding white debug indicators for scenes (named slide)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(controller);
+
+    //new animation
+    const pageTl = gsap.timeline();
+    //check to see if slides exist after current slide
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    //corrects the transition of next slide to animation vertically sooner
+    pageTl.fromTo(nextSlide, { y: "0" }, { y: "50%" });
+    //fades and scales down elements on scroll
+    pageTl.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    //reverse the vertical animation above after short delay
+    pageTl.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+
+    //new scroll scene
+    //does above animation for full length and starts at top of slide
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "page",
+        indent: 200,
+      })
+      //setting pin anchors scroll in place so it looks like animations are occuring on the spot
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageTl)
+      .addTo(controller);
   });
 }
 
